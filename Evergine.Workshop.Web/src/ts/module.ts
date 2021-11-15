ddc86902e4a3a93864f52adc674bd730ebd01ade
Rodrigo Cabello <mrcabellom@gmail.com>
2021-11-15 14:12:57 +0100

    Add typescript files to web project

---
+++ Evergine.Workshop.Web/src/ts/module.ts
@@ -0,0 +1,25 @@
+﻿
+class EvergineModule {
+
+    canvas: HTMLCanvasElement;
+
+    constructor(module: object) {
+        Object.assign(this);
+    }
+
+    locateFile(base: string) {
+        return `evergine/${base}`;
+    }
+
+    setProgress(progress: number) {
+        let percentage = Math.round(progress);
+
+        let loadingBarBar = document.getElementById("loading-bar-percentage");
+        loadingBarBar.style.width = percentage + "%";
+
+        let loadingBar = document.getElementById("loading-bar");
+        if (percentage === 100) {
+            loadingBar.classList.add("progress-infinite");
+        }
+    }
+}
---
+++ Evergine.Workshop.Web/src/ts/program.ts
@@ -0,0 +1,25 @@
+﻿class Program {
+
+    assemblyName: string;
+    className: string;
+
+    constructor(assemblyName: string, className: string) {
+        this.assemblyName = assemblyName;
+        this.className = className;
+    }
+
+    Run(canvasId: string) {
+        this.invoke("Run", canvasId);
+    }
+
+    UpdateCanvasSize(canvasId: string) {
+        this.invoke("UpdateCanvasSize", canvasId);
+
+    }
+    invoke(methodName: string, ...args: any[]) {
+        window.BINDING.call_static_method(
+            `[${this.assemblyName}] ${this.className}:${methodName}`,
+            args
+        );
+    }
+}
